c7547e2107b772b768348f01b90a495012bf2245
Guilherme Augusto <guilherme.bublitz@disruptiva.com.br>
2019-11-03 01:34:56 -0200

    Parcial commit profile screen

--- src/screens/profileScreen.js
+++ src/screens/profileScreen.js
@@ -1,25 +1,36 @@
 import React, {Component} from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
 import {connect} from 'react-redux';
 import {Title, SimpleButton} from '../components';
 import {logout} from '../ducks/modules/authDuck';
+import {scale} from '../utils';
 
 class ProfileScreen extends Component {
   constructor(props) {
     super(props);
-    console.log(this.props);
   }
 
   render() {
     return (
-      <SafeAreaView>
+      <SafeAreaView style={styles.containerStyle}>
         <Title text="Usuário" />
-        <Text>{this.props.user}</Text>
-        <SimpleButton onPress={() => this.props.logout(this.props.navigation)}>Sair</SimpleButton>
+        <View style={{marginTop: scale(20), marginBottom: scale(45)}}>
+          <Text>{this.props.user}</Text>
+        </View>
+        <SimpleButton onPress={() => this.props.logout(this.props.navigation)}>
+          Sair
+        </SimpleButton>
       </SafeAreaView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  containerStyle: {
+    padding: scale(10),
+    justifyContent: 'center',
+  },
+});
 
 const mapStateToProps = state => ({
   user: state.auth.user,
