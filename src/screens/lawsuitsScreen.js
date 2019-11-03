b8ce190f24c8812be3cfcfe5f561b0e33e4924bb
Guilherme Augusto <guilherme.bublitz@disruptiva.com.br>
2019-11-02 22:43:56 -0300

    Implements main lawsuitScreen component

---
+++ src/screens/lawsuitsScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const LawsuitsScreen = () => (
+  <View>
+    <Text>PROCESSOS</Text>
+  </View>
+);
+
+export default LawsuitsScreen;
