dd4db9175e8c889bd7d58e3d8a59194896427aae
Guilherme Augusto <guilherme.bublitz@disruptiva.com.br>
2019-11-02 23:56:06 -0300

    Implements logout action

--- src/ducks/modules/authDuck.js
+++ src/ducks/modules/authDuck.js
@@ -1,6 +1,9 @@
+import {NavigationActions, StackActions} from 'react-navigation';
+
 const Types = {
   LOGIN: 'auth/LOGIN',
   LOGIN_SUCCESS: 'auth/LOGIN_SUCCESS',
+  LOGOUT: 'auth/LOGOUT',
 };
 
 const INITIAL_STATE = {
@@ -11,11 +14,11 @@ const INITIAL_STATE = {
 export default function reducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case Types.LOGIN:
-      console.log('LOGIN');
       return {...state, loading: true};
     case Types.LOGIN_SUCCESS:
-      console.log('SUCCESS');
-      return {...state, loading: false};
+      return {...state, loading: false, email: action.payload.email};
+    case Types.LOGOUT:
+      return {...state, email: '', loading: false};
     default:
       return state;
   }
@@ -27,7 +30,19 @@ export function login(email, password) {
     dispatch({type: Types.LOGIN});
 
     setTimeout(function() {
-      dispatch({type: Types.LOGIN_SUCCESS});
+      dispatch({type: Types.LOGIN_SUCCESS, payload: {email}});
     }, 3000);
   };
+}
+
+export function logout(navigation) {
+  return dispatch => {
+    dispatch({type: Types.LOGOUT});
+    navigation.dispatch(
+      StackActions.reset({
+        index: 0,
+        actions: [NavigationActions.navigate({routeName: 'Login'})],
+      }),
+    );
+  };;
 }
